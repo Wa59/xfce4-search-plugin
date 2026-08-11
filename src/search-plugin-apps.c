62671e416aca300f963e74c16ec2afa4ae7ecaea
Wesley Daflita <wesley.daflita@gmail.com>
2026-08-11 19:46:25 -0300

    Search suggestions from bash history

--- src/search-plugin-apps.c
+++ src/search-plugin-apps.c
@@ -1,5 +1,239 @@
 #include "search-plugin.h"
 
+#include <stdio.h>
+#include <time.h>
+
+static gchar *search_plugin_get_shell_history_path(void);
+static gchar *search_plugin_quote_fish_history_cmd(const gchar *cmd);
+static void search_plugin_append_to_shell_history(const gchar *command);
+static GList *search_plugin_find_fish_history_commands(const gchar *query);
+static GList *search_plugin_find_path_commands(const gchar *query);
+
+static gchar *
+search_plugin_get_shell_history_path(void)
+{
+    const gchar *shell = g_getenv("SHELL");
+    const gchar *home = g_get_home_dir();
+
+    if (shell != NULL && g_str_has_suffix(shell, "fish"))
+        return g_build_filename(home, ".local", "share", "fish", "fish_history", NULL);
+
+    return g_build_filename(home, ".bash_history", NULL);
+}
+
+static gchar *
+search_plugin_quote_fish_history_cmd(const gchar *cmd)
+{
+    if (cmd == NULL)
+        return g_strdup("''");
+
+    GString *escaped = g_string_new(NULL);
+    for (const gchar *p = cmd; *p != '\0'; p++)
+    {
+        if (*p == '\'')
+            g_string_append(escaped, "''");
+        else
+            g_string_append_c(escaped, *p);
+    }
+
+    gchar *quoted = g_strdup_printf("'%s'", escaped->str);
+    g_string_free(escaped, TRUE);
+    return quoted;
+}
+
+static void
+search_plugin_append_to_shell_history(const gchar *command)
+{
+    if (command == NULL || g_strstrip((gchar *) command)[0] == '\0')
+        return;
+
+    gchar *history_path = search_plugin_get_shell_history_path();
+    if (history_path == NULL)
+        return;
+
+    FILE *file = fopen(history_path, "a");
+    if (file == NULL)
+    {
+        g_warning("Unable to open shell history file: %s", history_path);
+        g_free(history_path);
+        return;
+    }
+
+    if (g_str_has_suffix(history_path, "fish_history"))
+    {
+        gchar *escaped_command = search_plugin_quote_fish_history_cmd(command);
+        gint64 when = g_get_real_time() / G_USEC_PER_SEC;
+        fprintf(file, "- cmd: %s\n  when: %lld\n", escaped_command, (long long) when);
+        g_free(escaped_command);
+    }
+    else
+    {
+        fprintf(file, "%s\n", command);
+    }
+
+    fclose(file);
+    g_free(history_path);
+}
+
+static GList *
+search_plugin_find_path_commands(const gchar *query)
+{
+    if (query == NULL || query[0] == '\0')
+        return NULL;
+
+    gchar *normalized_query = g_utf8_casefold(query, -1);
+    gchar **parts = g_strsplit_set(normalized_query, " \t", 2);
+    gchar *search_term = g_strstrip(parts[0]);
+    if (search_term == NULL || search_term[0] == '\0')
+    {
+        g_strfreev(parts);
+        g_free(normalized_query);
+        return NULL;
+    }
+
+    const gchar *path_env = g_getenv("PATH");
+    if (path_env == NULL)
+    {
+        g_strfreev(parts);
+        g_free(normalized_query);
+        return NULL;
+    }
+
+    gchar **path_dirs = g_strsplit(path_env, ":", -1);
+    GHashTable *unique = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
+    GList *matches = NULL;
+
+    for (gint i = 0; path_dirs[i] != NULL; i++)
+    {
+        const gchar *dir = path_dirs[i];
+        if (dir[0] == '\0')
+            continue;
+
+        GDir *gdir = g_dir_open(dir, 0, NULL);
+        if (gdir == NULL)
+            continue;
+
+        const gchar *entry_name;
+        while ((entry_name = g_dir_read_name(gdir)) != NULL)
+        {
+            gchar *lower_name = g_utf8_casefold(entry_name, -1);
+            if (!g_str_has_prefix(lower_name, search_term))
+            {
+                g_free(lower_name);
+                continue;
+            }
+
+            gchar *full_path = g_build_filename(dir, entry_name, NULL);
+            if (g_file_test(full_path, G_FILE_TEST_IS_EXECUTABLE) && !g_file_test(full_path, G_FILE_TEST_IS_DIR))
+            {
+                if (!g_hash_table_contains(unique, entry_name))
+                {
+                    gchar *command = g_strdup(entry_name);
+                    g_hash_table_add(unique, g_strdup(entry_name));
+                    matches = g_list_prepend(matches, command);
+                }
+            }
+            g_free(full_path);
+            g_free(lower_name);
+        }
+
+        g_dir_close(gdir);
+    }
+
+    g_strfreev(path_dirs);
+    g_strfreev(parts);
+    g_free(normalized_query);
+    g_hash_table_destroy(unique);
+
+    return g_list_reverse(matches);
+}
+
+static GList *
+search_plugin_find_fish_history_commands(const gchar *query)
+{
+    if (query == NULL || query[0] == '\0')
+        return NULL;
+
+    gchar *history_path = search_plugin_get_shell_history_path();
+    if (history_path == NULL || !g_str_has_suffix(history_path, "fish_history"))
+    {
+        g_free(history_path);
+        return NULL;
+    }
+
+    gchar *contents = NULL;
+    GError *error = NULL;
+    if (!g_file_get_contents(history_path, &contents, NULL, &error))
+    {
+        g_warning("Unable to read fish history: %s", error != NULL ? error->message : "unknown error");
+        if (error != NULL)
+            g_error_free(error);
+        g_free(history_path);
+        return NULL;
+    }
+
+    gchar *normalized_query = g_utf8_casefold(query, -1);
+    gchar **lines = g_strsplit(contents, "\n", -1);
+    GHashTable *seen = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
+    GList *matches = NULL;
+
+    for (gint i = g_strv_length(lines) - 1; i >= 0; i--)
+    {
+        gchar *line = g_strstrip(lines[i]);
+        if (!g_str_has_prefix(line, "- cmd:"))
+            continue;
+
+        gchar *cmd_value = g_strstrip(line + 6);
+        if (cmd_value[0] == '\0')
+            continue;
+
+        gchar *command = NULL;
+        if (cmd_value[0] == '\'' && cmd_value[strlen(cmd_value) - 1] == '\'')
+        {
+            gchar *inner = g_strndup(cmd_value + 1, strlen(cmd_value) - 2);
+            GString *unescaped = g_string_new(NULL);
+            for (const gchar *p = inner; *p != '\0'; )
+            {
+                if (*p == '\'' && *(p + 1) == '\'')
+                {
+                    g_string_append_c(unescaped, '\'');
+                    p += 2;
+                }
+                else
+                {
+                    g_string_append_c(unescaped, *p);
+                    p++;
+                }
+            }
+            command = g_string_free(unescaped, FALSE);
+            g_free(inner);
+        }
+        else
+        {
+            command = g_strdup(cmd_value);
+        }
+
+        gchar *command_lower = g_utf8_casefold(command, -1);
+        if (g_str_has_prefix(command_lower, normalized_query) && !g_hash_table_contains(seen, command))
+        {
+            g_hash_table_add(seen, g_strdup(command));
+            matches = g_list_prepend(matches, g_strdup(command));
+        }
+
+        g_free(command_lower);
+        g_free(command);
+    }
+
+    g_free(contents);
+    g_free(history_path);
+    g_strfreev(lines);
+    g_free(normalized_query);
+    g_hash_table_destroy(seen);
+
+    return g_list_reverse(matches);
+}
+
+
 gboolean search_plugin_matches_query(GAppInfo *app_info, const gchar *query)
 {
     if (app_info == NULL || query == NULL || query[0] == '\0')
@@ -129,6 +363,7 @@ void search_plugin_launch_query(SearchPluginData *data, const gchar *query)
     GError *error = NULL;
     if (g_spawn_command_line_async(query, &error))
     {
+        search_plugin_append_to_shell_history(query);
         return;
     }
 
@@ -188,6 +423,38 @@ void search_plugin_refresh_popup(SearchPluginData *data)
     }
 
     g_list_free_full(app_infos, g_object_unref);
+
+    GList *history_matches = search_plugin_find_fish_history_commands(query);
+    if (history_matches != NULL)
+    {
+        GtkIconTheme *theme = gtk_icon_theme_get_default();
+        GdkPixbuf *pixbuf = gtk_icon_theme_load_icon(theme, "document-open-recent", SEARCH_PLUGIN_ICON_SIZE, GTK_ICON_LOOKUP_FORCE_SIZE, NULL);
+        for (GList *iter = history_matches; iter != NULL; iter = iter->next)
+        {
+            const gchar *command = iter->data;
+            search_plugin_add_result(store, command, NULL, command, pixbuf);
+            found_match = TRUE;
+        }
+        if (pixbuf != NULL)
+            g_object_unref(pixbuf);
+        g_list_free_full(history_matches, g_free);
+    }
+
+    GList *path_matches = search_plugin_find_path_commands(query);
+    if (path_matches != NULL)
+    {
+        GtkIconTheme *theme = gtk_icon_theme_get_default();
+        GdkPixbuf *pixbuf = gtk_icon_theme_load_icon(theme, "system-run-symbolic", SEARCH_PLUGIN_ICON_SIZE, GTK_ICON_LOOKUP_FORCE_SIZE, NULL);
+        for (GList *iter = path_matches; iter != NULL; iter = iter->next)
+        {
+            const gchar *command = iter->data;
+            search_plugin_add_result(store, command, NULL, command, pixbuf);
+            found_match = TRUE;
+        }
+        if (pixbuf != NULL)
+            g_object_unref(pixbuf);
+        g_list_free_full(path_matches, g_free);
+    }
 
     if (!found_match)
     {
